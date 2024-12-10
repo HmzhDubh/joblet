a7c19ac970ba0ca743c9e4a7e2f026292a979b87
HmzhDubh <meezo.2015@hotmail.com>
2024-12-10 14:32:18 +0300

    edit sign in, sign up view and forms styles

--- Joblet/accounts/views.py
+++ Joblet/accounts/views.py
@@ -1,16 +1,35 @@
+from django.contrib.auth.models import Group
+from django.db import transaction
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm, SignInForm
+from organization.models import Organization
 # Create your views here.
 
 
-def signup_view(request):
+def signup_view(request: HttpRequest):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)  
+            with transaction.atomic():
+                user = form.save()
+                login(request, user)
+                if request.POST['acc_type'] == 'candidate':
+                    my_group = Group.objects.get(name='candidate')
+                    my_group.user_set.add(user)
+                    # init the user profile todo
+                elif request.POST['acc_type'] == 'organization':
+                    my_group = Group.objects.get(name='organizations')
+                    my_group.user_set.add(user)
+                    # init the organization profile
+                    new_organization = Organization(
+                        profile = request.user,
+                        email = request.POST['email']
+                    )
+                    new_organization.save()
+
             return redirect('main:home_view')
     else:
         form = SignUpForm()
