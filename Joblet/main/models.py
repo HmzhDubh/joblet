c04da666ece3fbe1ebda533cfde40500a60ad80b
HmzhDubh <meezo.2015@hotmail.com>
2024-12-12 00:30:39 +0300

    add contacts, and update home view

--- Joblet/main/models.py
+++ Joblet/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    subject = models.CharField(max_length=100)
+    contact_message = models.TextField()
